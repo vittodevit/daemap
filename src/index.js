5d5ad927ebf4b7875ba6a0fb429b5baf98c659f2
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-04-03 19:14:11 +0200

    primo commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.js';
+import Homepage from './pages/homepage/Homepage';
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(
+  <React.StrictMode>
+    <Homepage />
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { 
+  MapContainer, 
+  TileLayer, 
+  Marker, 
+  Popup 
+} from 'react-leaflet'
+import './Homepage.css';
+
+function Homepage() {
+  return (
+    // https://www.google.it/maps/@41.6300921,15.9133647,14.63z
+    <MapContainer center={[41.6300921, 15.9133647]} zoom={14} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | 
+        DaeMap by <a href="https://vitto.dev">Vittorio Lo Mele</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default Homepage;

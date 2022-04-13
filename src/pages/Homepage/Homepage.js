deefa907b2af6b04d206ba316926d050e8678b0c
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-04-13 21:56:23 +0200

    PWA and icons

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -5,7 +5,7 @@ import LeafletMap from "../../components/LeafletMap/LeafletMap";
 function Homepage() {
   return (
     // MANFREDONIA: https://www.google.it/maps/@41.6300921,15.9133647,14.63z
-    <LeafletMap centerLat="41.6300921" centerLon="15.9133647" zoomLevel="14" />
+    <LeafletMap centerLat="41.6200921" centerLon="15.9133647" zoomLevel="15" />
   );
 }
 

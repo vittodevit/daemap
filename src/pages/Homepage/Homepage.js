4c20718df0cee96c2a4daba5cc1f7fd7e0d96023
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-05-11 10:42:08 +0200

    auto push update

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Homepage.css";
 import LeafletMap from "../../components/LeafletMap/LeafletMap";
+import ServiceWorkerWrapper from "../../components/ServiceWorkerWrapper/ServiceWorkerWrapper";
 
 function Homepage() {
   return (
     // MANFREDONIA: https://www.google.it/maps/@41.6300921,15.9133647,14.63z
-    <LeafletMap centerLat="41.6200921" centerLon="15.9133647" zoomLevel="15" />
+    <div>
+      <LeafletMap centerLat="41.6200921" centerLon="15.9133647" zoomLevel="15" />
+      <ServiceWorkerWrapper></ServiceWorkerWrapper>
+    </div>
   );
 }
 
--- src/service-worker.js
+++ src/service-worker.js
@@ -70,3 +70,4 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+console.log("VERSION 0.1.1")

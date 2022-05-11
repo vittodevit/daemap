e22605894d273ed6c1a73cac25cfe52a1d21e6c0
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-05-11 10:19:32 +0200

    eliminazione immagini da directoty upload

--- cms/admin/api/delete.php
+++ cms/admin/api/delete.php
@@ -13,9 +13,15 @@ if(!isset($_POST["id"]) || empty($_POST["id"])){
 
 $id = $con->real_escape_string($_POST["id"]);
 $tn = $config['DB_TABLE'];
+
+// retrieve image id imageId
+$res1 = $con->query("SELECT `imageId` FROM `$tn` WHERE ((`id` = '$id'));");
+$row1 = $res1->fetch_assoc();
+
 $res = $con->query("DELETE FROM `$tn` WHERE ((`id` = '$id'));");
 
 if($con->affected_rows > 0){
+    unlink("../../api/images/" . $row1["imageId"] . ".webp");
     die('AJAX: OK');
 }else{
     http_response_code(404);
